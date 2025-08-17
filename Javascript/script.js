a3acb46c2c7eeaaf2cab0c14707edb763a15e286
Adhil T <adhitmkm.com>
2025-08-17 13:38:44 +0530

    uploading puma clone

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,16 +1,12 @@
-
-var x = 1 
-a();
-b();
-console.log(x);
-
-
-function a() {
-    var x = 10;
-    console.log(x)
+{
+    var a = 10;
+    let b = 20;
+    const c = 30;
+    console.log(a)
+    console.log(b)
+    console.log(c)
 }
 
-function b() {
-    var x = 100;
-    console.log(x)
-}
+    console.log(a)
+    console.log(b)
+    console.log(c)
